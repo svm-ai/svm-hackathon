65d7cad471934ec2ca25f1b87c618305240fe81a
Younesse Kaddar <younesse.kaddar@chch.ox.ac.uk>
2023-06-04 17:51:30 +0000

    Erinc's updates

--- proteinbind_new.py
+++ proteinbind_new.py
@@ -15,6 +15,13 @@ ModalityType = SimpleNamespace(
     TEXT="text",
 )
 
+class Normalize(nn.Module):
+    def __init__(self, dim: int) -> None:
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return torch.nn.functional.normalize(x, dim=self.dim, p=2)
 
 class EmbeddingDataset(Dataset):
     """
@@ -81,8 +88,121 @@ class ProteinBindModel(nn.Module):
             in_embed_dim,
             out_embed_dim,
         )
-        self._create_modality_postprocessors()
-        pass
+        self.modality_postprocessors = self._create_modality_postprocessors(
+            out_embed_dim
+        )
+
+
+    def _create_modality_trunk(
+            self,
+            aa_embed_dim,
+            dna_embed_dim,
+            pdb_embed_dim,
+... (183 lines left)
+Collapse
+proteinbind_new.py
+9 KB
+youkad — Today at 18:31
+Erinc, can we do some pair-coding in 10-ish minutes? (I need fix a problem I have with VSCode rigtht now)
+erinc — Today at 18:32
+sure I ping you I will do quick chat with UI guys
+erinc — Today at 18:44
+I am free now
+﻿
+from types import SimpleNamespace
+
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+
+ModalityType = SimpleNamespace(
+    AA="aa",
+    DNA="dna",
+    PDB="pdb",
+    GO="go",
+    MSA="msa",
+    TEXT="text",
+)
+
+class Normalize(nn.Module):
+    def __init__(self, dim: int) -> None:
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return torch.nn.functional.normalize(x, dim=self.dim, p=2)
+
+class EmbeddingDataset(Dataset):
+    """
+    The main class for turning any modality to a torch Dataset that can be passed to
+    a torch dataloader. Any modality that doesn't fit into the __getitem__
+    method can subclass this and modify the __getitem__ method.
+    """
+    def __init__(self, sequence_file_path, embeddings_file_path, modality):
+        self.sequence = pd.read_csv(sequence_file_path)
+        self.embedding = torch.load(embeddings_file_path)
+        self.modality = modality
+
+    def __len__(self):
+        return len(self.sequence)
+
+    def __getitem__(self, idx):
+        sequence = self.sequence.iloc[idx, 0]
+        embedding = self.embedding[idx]
+        return {"aa": sequence, self.modality: embedding}
+
+class DualEmbeddingDataset(Dataset):
+    """
+    The main class for turning any modality to a torch Dataset that can be passed to
+    a torch dataloader. Any modality that doesn't fit into the __getitem__
+    method can subclass this and modify the __getitem__ method.
+    """
+    def __init__(self, sequence_embeddings_file_path, embeddings_file_path, modality):
+        self.sequence_embedding = torch.load(sequence_embeddings_file_path)
+        self.embedding = torch.load(embeddings_file_path)
+        self.modality = modality
+
+    def __len__(self):
+        return len(self.sequence_embedding)
+
+    def __getitem__(self, idx):
+        sequence_embedding = self.sequence_embedding[idx]
+        embedding = self.embedding[idx]
+        return {"aa": sequence_embedding, self.modality: embedding}
+    
+class ProteinBindModel(nn.Module):
+
+    def __init__(
+            self,
+            aa_embed_dim,
+            dna_embed_dim,
+            pdb_embed_dim,
+            go_embed_dim,
+            msa_embed_dim,
+            text_embed_dim,
+            in_embed_dim,
+            out_embed_dim
+    ):
+        super().__init__()
+        self.modality_trunks = self._create_modality_trunk(
+            aa_embed_dim,
+            dna_embed_dim,
+            pdb_embed_dim,
+            go_embed_dim,
+            msa_embed_dim,
+            text_embed_dim,
+            out_embed_dim
+        )
+        self.modality_heads = self._create_modality_head(
+            in_embed_dim,
+            out_embed_dim,
+        )
+        self.modality_postprocessors = self._create_modality_postprocessors(
+            out_embed_dim
+        )
+
 
     def _create_modality_trunk(
             self,
@@ -167,37 +287,52 @@ class ProteinBindModel(nn.Module):
 
         modality_heads[ModalityType.AA] = nn.Sequential(
             nn.LayerNorm(normalized_shape=in_embed_dim, eps=1e-6),
+            nn.Dropout(p=0.1),
             nn.Linear(in_embed_dim, out_embed_dim, bias=False),
         )
 
         modality_heads[ModalityType.DNA] = nn.Sequential(
             nn.LayerNorm(normalized_shape=in_embed_dim, eps=1e-6),
+            nn.Dropout(p=0.1),
             nn.Linear(in_embed_dim, out_embed_dim, bias=False),
         )
 
         modality_heads[ModalityType.PDB] = nn.Sequential(
             nn.LayerNorm(normalized_shape=in_embed_dim, eps=1e-6),
+            nn.Dropout(p=0.1),
             nn.Linear(in_embed_dim, out_embed_dim, bias=False),
         )
 
         modality_heads[ModalityType.GO] = nn.Sequential(
             nn.LayerNorm(normalized_shape=in_embed_dim, eps=1e-6),
+            nn.Dropout(p=0.1),
             nn.Linear(in_embed_dim, out_embed_dim, bias=False),
         )
 
         modality_heads[ModalityType.MSA] = nn.Sequential(
             nn.LayerNorm(normalized_shape=in_embed_dim, eps=1e-6),
+            nn.Dropout(p=0.1),
             nn.Linear(in_embed_dim, out_embed_dim, bias=False),
         )
 
         modality_heads[ModalityType.TEXT] = nn.Sequential(
             nn.LayerNorm(normalized_shape=in_embed_dim, eps=1e-6),
+            nn.Dropout(p=0.1),
             nn.Linear(in_embed_dim, out_embed_dim, bias=False),
         )
         return nn.ModuleDict(modality_heads)
 
-    def _create_modality_postprocessors(self):
-        pass
+    def _create_modality_postprocessors(self, out_embed_dim):
+        modality_postprocessors = {}
+        modality_postprocessors[ModalityType.AA] = Normalize(dim=-1)
+        modality_postprocessors[ModalityType.DNA] = Normalize(dim=-1)
+        modality_postprocessors[ModalityType.PDB] = Normalize(dim=-1)
+        modality_postprocessors[ModalityType.TEXT] = Normalize(dim=-1)
+        modality_postprocessors[ModalityType.GO] = Normalize(dim=-1)
+        modality_postprocessors[ModalityType.MSA] = Normalize(dim=-1)
+
+
+        return nn.ModuleDict(modality_postprocessors)
 
     def forward(self, inputs):
         """
@@ -216,15 +351,18 @@ class ProteinBindModel(nn.Module):
         for modality_key, modality_value in inputs.items():
 
 
-            modality_trunk_output = self.modality_trunks[modality_key](
+            modality_value = self.modality_trunks[modality_key](
                 modality_value
             )
 
-            modality_embedding_value = self.modality_heads[modality_key](
-                modality_trunk_output
+            modality_value = self.modality_heads[modality_key](
+                modality_value
             )
-
-            outputs[modality_key] = modality_embedding_value
+             
+            modality_value = self.modality_postprocessors[modality_key](
+                    modality_value
+                )
+            outputs[modality_key] = modality_value
 
         return outputs
 
@@ -242,13 +380,13 @@ def create_proteinbind(pretrained=False):
         go_embed_dim=600,
         msa_embed_dim=768,
         text_embed_dim=768,
-        in_embed_dim=128,
-        out_embed_dim=128
+        in_embed_dim=256,
+        out_embed_dim=256
     )
 
     if pretrained:
         #get path from config
-        PATH = ''
+        PATH = 'best_model.pth'
 
         model.load_state_dict(torch.load(PATH))
 
